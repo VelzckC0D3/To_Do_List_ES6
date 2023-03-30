df3e02f18dccd47e8a08c6456c578b711dde65ff
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-03-30 02:35:05 -0500

    Functionalities & Style
    - Added an extra module
    - Added "list" class to iterate through the list
    - The list is created dynamically
    - Added remove function
    - Added add function
    - Added edit function
    - All the changes on the array are save in the local storage

---
+++ src/modules/list.js
@@ -0,0 +1,202 @@
+class List {
+  // Establish how the task are going to be created inside the list
+  constructor() {
+    this.list = [];
+    this.length = 0;
+    const storedList = localStorage.getItem('list');
+    if (storedList) {
+      const parsedList = JSON.parse(storedList);
+      this.length = parsedList[parsedList.length - 1].index;
+    }
+  }
+
+  // Add a new task to the List constructor
+  addTask() {
+    const addTask = document.querySelector('#toDo');
+    const addInput = document.querySelector('.addToInput');
+    const addTaskIcon = document.querySelector('#addIcon');
+
+    // Saved when pressed "Enter" Key
+    addTask.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        const inputValue = addInput.value;
+        if (inputValue !== '') {
+          const description = inputValue;
+          const completed = false;
+          const index = this.list.length > 0
+            ? this.list[this.list.length - 1].index + 1
+            : 1;
+          /* console.log(this.list); */
+          this.saveTask(description, completed, index);
+          e.preventDefault();
+          this.showList();
+
+          // Animate the icon when clicking
+          const addTaskIcon = document.querySelector('#addIcon');
+          addTaskIcon.classList.add('addIconEnter');
+          setTimeout(() => {
+            addTaskIcon.classList.remove('addIconEnter');
+          }, 500);
+
+          addInput.value = '';
+        } else {
+          e.preventDefault();
+          const addTaskIcon = document.querySelector('#addIcon');
+          addTaskIcon.classList.add('addIconError');
+          setTimeout(() => {
+            addTaskIcon.classList.remove('addIconError');
+          }, 500);
+        }
+      }
+    });
+
+    // Saved wen pressed "+" Icon
+    addTaskIcon.addEventListener('click', () => {
+      const inputValue = addInput.value;
+      if (inputValue !== '') {
+        const description = inputValue;
+        const completed = false;
+        const index = this.list.length > 0 ? this.list[this.list.length - 1].index + 1 : 1;
+        /* console.log(this.list); */
+        this.saveTask(description, completed, index);
+        this.showList();
+        addInput.value = '';
+      }
+    });
+  }
+
+  // Remove a Task after clicking the remove icon
+  removeTask() {
+    const listGroup = document.querySelector('.toDoList');
+    listGroup.addEventListener('click', (event) => {
+      const removeIcon = event.target.closest('.removeIcon');
+      if (removeIcon) {
+        const listItem = removeIcon.closest('.listItem');
+        const indexToRemove = parseInt(listItem.id, 10);
+        const filteredList = this.list.filter(
+          (task) => task.index !== indexToRemove,
+        );
+        this.list = filteredList;
+
+        // Check if list is empty and delete it
+        if (this.list.length === 0) {
+          localStorage.removeItem('list');
+        } else {
+          // Update the index of the remaining tasks
+          this.list.forEach((task, i) => {
+            task.index = i + 1;
+          });
+          // Update the local storage
+          localStorage.setItem('list', JSON.stringify(this.list));
+        }
+        // Update the list
+        this.showList();
+      }
+    });
+  }
+
+  // Edit Task
+  editTask() {
+    const listGroup = document.querySelector('.toDoList');
+    listGroup.addEventListener('keydown', (event) => {
+      const editInput = event.target.closest('.toDoTask');
+      if (editInput && event.key === 'Enter') {
+        const listItem = editInput.closest('.listItem');
+        const indexToEdit = parseInt(listItem.id, 10);
+        const updatedDescription = editInput.value;
+        const updatedTask = this.list.find(
+          (task) => task.index === indexToEdit,
+        );
+        updatedTask.description = updatedDescription;
+        localStorage.setItem('list', JSON.stringify(this.list));
+        this.showList();
+      }
+    });
+    listGroup.addEventListener('focusout', (event) => {
+      const editInput = event.target.closest('.toDoTask');
+      if (editInput && document.activeElement !== editInput) {
+        const listItem = editInput.closest('.listItem');
+        const indexToEdit = parseInt(listItem.id, 10);
+        const updatedDescription = editInput.value;
+        const updatedTask = this.list.find(
+          (task) => task.index === indexToEdit,
+        );
+        updatedTask.description = updatedDescription;
+        localStorage.setItem('list', JSON.stringify(this.list));
+        this.showList();
+      }
+    });
+  }
+
+  // Save the task on the local storage
+  saveTask(description, completed, index) {
+    const newTask = { description, completed, index };
+    this.list.push(newTask);
+    if (!localStorage.getItem('list')) {
+      localStorage.setItem('list', JSON.stringify(this.list));
+    } else {
+      const savedList = JSON.parse(localStorage.getItem('list'));
+      savedList.push(newTask);
+      localStorage.setItem('list', JSON.stringify(savedList));
+    }
+  }
+
+  // Save the List of task on the Local storage
+  loadTask() {
+    if (localStorage.getItem('list')) {
+      const savedList = JSON.parse(localStorage.getItem('list'));
+      savedList.forEach((task) => {
+        this.list.push({
+          description: task.description,
+          completed: task.completed,
+          index: task.index,
+        });
+      });
+    }
+  }
+
+  // Show List
+  showList() {
+    // Add the list to the HTML
+    const listGroup = document.querySelector('.toDoList');
+    listGroup.innerHTML = '';
+    this.list.forEach((task) => {
+      const listTask = document.createElement('li');
+      listTask.classList = 'listItem listElement';
+      listTask.id = `${task.index}`;
+      listTask.innerHTML = `
+        <input type="checkbox" class="checkBox" ${
+  task.completed ? 'checked' : ''
+}>
+        <input type="text" class="toDoTask" value="${task.description}">
+        <i class="fa-regular fa-trash-can removeIcon"></i>
+        <i class="fa-solid fa-ellipsis-vertical moveIcon"></i>
+      `;
+
+      // Checkbox function to change the status of "completed"
+      const checkbox = listTask.querySelector('.checkBox');
+      const actualTask = listTask.querySelector('.toDoTask');
+      if (checkbox.checked) {
+        listTask.classList.add('checked');
+        actualTask.classList.add('taskChecked');
+      }
+      checkbox.addEventListener('click', () => {
+        task.completed = checkbox.checked;
+        if (checkbox.checked) {
+          listTask.classList.add('checked');
+          actualTask.classList.add('taskChecked');
+          /* console.log(task); */
+        } else {
+          listTask.classList.remove('checked');
+          actualTask.classList.remove('taskChecked');
+          /* console.log(task); */
+        }
+        localStorage.setItem('list', JSON.stringify(this.list));
+      });
+
+      listGroup.appendChild(listTask);
+    });
+  }
+}
+
+export default List;
